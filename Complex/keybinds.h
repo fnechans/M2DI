2193d49b4b1219161b538506834a2076b713eb37
fnechans <filip.nechansky@protonmail.com>
2025-10-12 19:11:34 +0200

    Add keybinds

---
+++ Complex/keybinds.h
@@ -0,0 +1,64 @@
+#ifndef KEYBINDS_H
+#define KEYBINDS_H
+
+#include "SDL.h"
+
+struct KeyBind
+{
+    KeyBind(SDL_Keycode key,
+        std::function<void()> func_down,
+        std::function<void()> func_up
+    ) : key(key), func_down(func_down), func_up(func_up) {}
+    SDL_Keycode key;
+    std::function<void()> func_down;
+    std::function<void()> func_up;
+};
+
+class KeyBinds
+{
+public:
+    void add_keybind(const std::string &name,
+            SDL_Keycode key,
+            std::function<void()> func_down,
+            std::function<void()> func_up)
+    {
+        if (binds.count(name) > 0)
+            throw std::runtime_error("Keybind of name " + name + " already "
+                                                                 "exists in the manager.");
+        binds.emplace(name, KeyBind(key, func_down, func_up));
+    }
+    void change_keybind(const std::string &name, SDL_Keycode key) { 
+        binds.at(name).key = key;
+    }
+
+    void evaluate(SDL_Event &events)
+    {
+        if (events.type == SDL_KEYDOWN && events.key.repeat == 0)
+        {
+            for (auto &bind : binds)
+            {
+                if (bind.second.key == events.key.keysym.sym)
+                {
+                    std::cout << "Keybind: " << bind.first << " pressed" << std::endl;
+                    bind.second.func_down();
+                }
+            }
+        }
+        else if (events.type == SDL_KEYUP && events.key.repeat == 0)
+        {
+            for (auto &bind : binds)
+            {
+                if (bind.second.key == events.key.keysym.sym)
+                {
+                    std::cout << "Keybind: " << bind.first << " released" << std::endl;
+                    bind.second.func_up();
+                }
+            }
+        }
+    }
+
+private:
+    std::map<std::string, KeyBind> binds;
+};
+
+#endif // KEYBINDS_H
